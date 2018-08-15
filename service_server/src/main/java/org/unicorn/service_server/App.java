5652eec3bae9bc24093ba1973f831b65a76f80ce
KING <CodeWalker-007@outlook.com>
2018-08-15 23:55:52 +0800

    init

---
+++ service_server/src/main/java/org/unicorn/service_server/App.java
@@ -0,0 +1,13 @@
+package org.unicorn.service_server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
