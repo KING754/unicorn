be415a8717fd9f6ec5bb3c6f9a859bbeb7e521d5
KING <CodeWalker-007@outlook.com>
2018-08-29 23:54:40 +0800

    netty的channel的编解码器的更换

--- unicorn_common/src/main/java/org/unicorn/common/netty/ServerChannel.java
+++ unicorn_common/src/main/java/org/unicorn/common/netty/ServerChannel.java
@@ -3,10 +3,7 @@ package org.unicorn.common.netty;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.string.StringDecoder;
-import io.netty.handler.codec.string.StringEncoder;
 import io.netty.handler.logging.LoggingHandler;
-import io.netty.util.CharsetUtil;
 
 /**
  * 通信信道工厂类
@@ -24,8 +21,8 @@ public class ServerChannel  extends ChannelInitializer<SocketChannel> {
        //netty自带了很多handler,LoggingHandler
        //用于做netty收发消息时的日志输出,后期可以关闭,前期打开用于调试
        pipeline.addLast("logger",new LoggingHandler());     
-       pipeline.addLast("decoder", new StringDecoder(CharsetUtil.UTF_8));  
-       pipeline.addLast("encoder", new StringEncoder(CharsetUtil.UTF_8));
+       pipeline.addLast("decoder",new PBDecoder());  
+       pipeline.addLast("encoder", new PBEncoder());
        pipeline.addLast("handler", new ServerIoHandler());
         
     }
